dad0767be032e6c8aced94a03adfc9796491a63f
AVVS <v@makeomatic.ca>
2022-03-15 22:06:57 +0000

    chore: multi-process scaling confirmed

--- packages/benchmarks/src/service-transport.js
+++ packages/benchmarks/src/service-transport.js
@@ -3,20 +3,25 @@
 // require("coffeescript/register");
 // require("source-map-support/register");
 
+// docker exec into tester
+// ./node_modules/.bin/clinic flame -- node ./src/service-transport.js
+
 (async () => {
   const AMQP = require('@microfleet/transport-amqp');
+  const { cpuUsage } = require('process');
 
-  const handleMessage = (data, properties, message, reply) => {
-    // do nothing
-    message.ack();
-    reply(null);
+  async function handleMessage(data, message) {
+    return { bye: true }
   };
 
+  const neck = 80
   const config = {
     exchange: 'bench.direct',
     queue: 'consume-test',
     listen: ['testing'],
-    neck: 100,
+    debug: true,
+    name: 'bench-test',
+    neck,
     exchangeArgs: {
       autoDelete: true,
       type: 'direct',
@@ -27,7 +32,22 @@
     },
   };
 
-  await AMQP.connect(config, handleMessage);
+  console.log('asking to connect')
+
+  const transport = await AMQP.connect(config, handleMessage);
+
+  // do nothing
+  let messages = 0;
+  const ackEvery = (neck / 2)
+  transport.on('after', (message) => {
+    messages += 1
+    if (messages % ackEvery === 0) {
+      messages = 0
+      message.multiAck()
+    }
+  })
+
+  console.log('connected %s', process.pid)
 })().catch((err) => {
   console.error(err);
   process.exit(128);
